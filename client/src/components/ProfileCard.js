17b66ba7561111017cd4934ae33e6205ebfef6ff
lucas <lucaskeller98@gmail.com>
2020-03-23 14:38:39 +0100

    renamed & optimized profileCard

--- client/src/components/ProfileCard.js
+++ client/src/components/ProfileCard.js
@@ -6,16 +6,9 @@ import HomeIcon from '@material-ui/icons/Home';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 
 
-class Profile extends Component {
+class ProfileCard extends Component {
     render() {
         return(
-            <Grid 
-                container 
-                spacing={0} 
-                alignItems="center"
-                justify="center"
-                style={{ height: '100vh' }}
-            >
             <Paper className="swipePaper">
                 <Grid container style={{margin: '10px', alignContent: 'flex-start'}} spacing={2}>
                     <Grid style={{height: 'fit-content', marginBottom: '1em'}} item xs={12}>
@@ -42,13 +35,10 @@ class Profile extends Component {
                         <Typography style={{fontSize: '16px', textAlign: 'justify'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
                         </Typography>
                     </Grid>
-                    
                 </Grid>
             </Paper>
-            </Grid>
-
         );
     }
 }
 
-export default Profile;
+export default ProfileCard;
