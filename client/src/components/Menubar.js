8168ea4beb31a20cde387f7b4c1831025f4007e9
lucas <lucaskeller98@gmail.com>
2020-04-02 13:27:06 +0200

    client: logout button & replaced reach router

--- client/src/components/Menubar.js
+++ client/src/components/Menubar.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
+import { AppBar, Toolbar, Typography, IconButton, Box } from '@material-ui/core';
 import AccountCircle from '@material-ui/icons/AccountCircle';
-import Box from '@material-ui/core/Box';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 import SettingsIcon from '@material-ui/icons/Settings';
-import { navigate } from '@reach/router';
+import { Link } from "react-router-dom";
+import { logout } from '../actions/auth'; 
 
 
 class Menubar extends Component {
@@ -17,19 +15,30 @@ class Menubar extends Component {
                     <div style={{ width: '100%' }}>
                         <Box display="flex">    
                             <Box display="flex" justifyContent="flex-start" style={{ width: '33.33%'}}>
-                                <IconButton color="inherit" aria-label="menu" onClick={() => navigate("/settings")}>
-                                    <SettingsIcon />
-                                </IconButton>
+                                <Link style={{color: 'white'}} to="/settings">
+                                    <IconButton color="inherit" aria-label="menu">
+                                        <SettingsIcon />
+                                    </IconButton>
+                                </Link>
                             </Box>
                             <Box display="flex" justifyContent="center" style={{ width: '33.33%'}}>
-                                <Typography variant="h5" style={{fontFamily: 'chaletmedium', fontSize: '40px'}}>
-                                    tinster
-                                </Typography>
+                                <Link style={{color: 'white', textDecoration: 'none'}} to="/home">
+                                    <Typography variant="h5" style={{fontFamily: 'chaletmedium', fontSize: '40px'}}>
+                                        tinster
+                                    </Typography>
+                                </Link>
                             </Box>
                             <Box display="flex" justifyContent="flex-end" style={{ width: '33.33%'}}>
-                                <IconButton color="inherit" aria-label="account of current user" aria-controls="menu-appbar" onClick={() => navigate("/profile")}>
-                                    <AccountCircle />
-                                </IconButton>
+                                <Link style={{color: 'white'}} to="/profile">
+                                    <IconButton color="inherit" aria-label="account of current user" aria-controls="menu-appbar">
+                                        <AccountCircle />
+                                    </IconButton>
+                                </Link>
+                                <Link style={{color: 'white'}} to="/">
+                                    <IconButton color="inherit" aria-label="logout" aria-controls="menu-appbar" onClick={() => logout()}>
+                                        <ExitToAppIcon />
+                                    </IconButton>
+                                </Link>
                             </Box>
                         </Box>
                     </div>
