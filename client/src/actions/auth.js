af463285e2fd882ac52214147db91ee3594dcbef
lucas <lucaskeller98@gmail.com>
2020-04-02 13:25:39 +0200

    client: axios auth header & token request

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,12 +1,28 @@
 import axios from 'axios';
 
+export const setAuhorizationToken = (token) => {
+  if(token){
+    // https://stackoverflow.com/questions/43051291/attach-authorization-header-for-all-axios-requests
+    axios.defaults.headers.common['Authorization'] = `Bearer ${token}`
+  } else {
+    delete axios.defaults.headers.common["Authorization"];
+  }
+}
+
+export const logout = () => {
+   localStorage.clear();
+   setAuhorizationToken(false)
+}
+
 
 export const signIn = async (username, password) => {
     const res = await axios.post('http://localhost:3001/api/auth/signIn', {
       username: username,
       password: password,
     });
-    console.log("res: ", res.data);
+    // console.log("res: ", res);
+    localStorage.setItem("jwtToken", res.data.token);
+    setAuhorizationToken(res.data.token);
     return res.data;
   };
 
@@ -18,7 +34,9 @@ export const signUp = async (email, username, password, gender, preference) => {
         gender: gender,
         preference: preference
     });
-    console.log("res: ", res.data);
+    // console.log("res: ", res);
+    localStorage.setItem("jwtToken", res.data.token);
+    setAuhorizationToken(res.data.token);
     return res.data;
 };
 
@@ -27,4 +45,12 @@ export const checkUsername = async username => {
     username: username
   });
   return res.data;
+}
+
+export const checkToken = async () => {
+  if (localStorage.jwtToken) {
+    setAuhorizationToken(localStorage.jwtToken);
+  }
+  const res = await axios.get('http://localhost:3001/api/auth/checkToken');
+  return res;
 }
